e8260b2dafc500921fe850d21cbf4eb2a824493b
Hackbright Student <info@hackbrightacademy.com>
2016-04-11 13:04:39 -0700

    More eloquent .get and .extend methods

--- wordcount.py
+++ wordcount.py
@@ -6,7 +6,7 @@ list_of_lines = []
 for line in text:
     line = line.rstrip()
     line = line.split(' ')
-    list_of_lines.append(line)
+    list_of_lines.extend(line)
 
 # return list_of_lines
 
@@ -14,13 +14,15 @@ for line in text:
 
 word_dict = {}
 
-for line in list_of_lines:
-    for word in line:
-        value = word_dict.get(word, 0)
-        if value == 0:
-            word_dict[word] = 1
-        else:
-            word_dict[word] = value + 1
+for word in list_of_lines:
+
+    value = word_dict.get(word, 0)
+    word_dict[word] = value + 1
+    
+    # if value == 0:
+    #     word_dict[word] = 1
+    # else:
+    #     word_dict[word] = value + 1
 
 for word, count in word_dict.items():
     print "%s %d" % (word, count)
